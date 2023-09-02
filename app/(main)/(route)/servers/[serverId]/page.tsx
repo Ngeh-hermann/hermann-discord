1f0bb801405859547b4097fa2f1dc8e4217f94b4
Ngeh-hermann <ngehhermann719@gmail.com>
2023-09-02 16:12:38 +0100

    Finished Channel Listing and MbileToggle

--- app/(main)/(route)/servers/[serverId]/page.tsx
+++ app/(main)/(route)/servers/[serverId]/page.tsx
@@ -1,7 +1,49 @@
-const ServerIDPage = () => {
-    return ( 
-        <div>Sever Page</div>
-     );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIDPageprops {
+  params: {
+    serverId: string;
+  };
 }
- 
+
+const ServerIDPage = async ({ params }: ServerIDPageprops) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "General",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "General") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+};
+
 export default ServerIDPage;
